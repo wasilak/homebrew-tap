f5a83063d2547080deafb805be8dbe00c1e71102
Piotr Boruc <piotr.m.boruc@gmail.com>
2026-05-13 08:23:32 +0200

    Add nim-cli formula and modernise tap to 2026 standards
    - Add nim-cli formula (v0.10.0) with conflicts_with nim, shell completions via generate_completions_from_executable, and caveats for alias setup
    - Rewrite consul_members_exporter and elasticsearch-reroute-shards to use modern on_macos/on_linux/on_intel/on_arm DSL (fixes brew style failures)
    - Fix consul_members_exporter: typo in desc, broken Intel Mac source tarball block removed, version pinned consistently at 0.0.3
    - Add license field to all three formulas
    - Fix renovate.json postUpdateOptions format (array of strings, not objects)
    - Add .gitignore to exclude bottle and test artefacts
    - Add macos-13 runner to tests.yml for explicit Intel Mac CI coverage

--- Formula/elasticsearch-reroute-shards.rb
+++ Formula/elasticsearch-reroute-shards.rb
@@ -2,31 +2,29 @@ class ElasticsearchRerouteShards < Formula
   desc "Elasticsearch shard rebalancing tool"
   homepage "https://github.com/wasilak/elasticsearch-reroute-shards"
   version "1.0.0"
+  license "MIT"
 
-  if OS.mac?
-    url "https://github.com/wasilak/elasticsearch-reroute-shards/releases/download/v1.0.0/elasticsearch-reroute-shards-darwin-amd64.zip"
-    sha256 "7e49326ea7bb38c49d33a60f13b311a01f16ca3ef222d1563292f978905ced13"
+  on_macos do
+    on_intel do
+      url "https://github.com/wasilak/elasticsearch-reroute-shards/releases/download/v1.0.0/elasticsearch-reroute-shards-darwin-amd64.zip"
+      sha256 "7e49326ea7bb38c49d33a60f13b311a01f16ca3ef222d1563292f978905ced13"
+    end
+    on_arm do
+      url "https://github.com/wasilak/elasticsearch-reroute-shards/releases/download/v1.0.0/elasticsearch-reroute-shards-darwin-arm64.zip"
+      sha256 "e371a1bba33eaf369ce379f336732f5f7bdce5f3df51234afb03c1975f52a082"
+    end
   end
 
-  if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/wasilak/elasticsearch-reroute-shards/releases/download/v1.0.0/elasticsearch-reroute-shards-darwin-arm64.zip"
-    sha256 "e371a1bba33eaf369ce379f336732f5f7bdce5f3df51234afb03c1975f52a082"
+  on_linux do
+    on_intel do
+      url "https://github.com/wasilak/elasticsearch-reroute-shards/releases/download/v1.0.0/elasticsearch-reroute-shards-linux-amd64.zip"
+      sha256 "39c2a6f5ac7eeb4f1102f53ae877ec2d6d9977b1c122b9512dac4b56bc235846"
+    end
+    on_arm do
+      url "https://github.com/wasilak/elasticsearch-reroute-shards/releases/download/v1.0.0/elasticsearch-reroute-shards-linux-arm64.zip"
+      sha256 "18a00e9ab8a58a999fcfcc4ef121af6c23f6c07f489005c350c3016d94c4e328"
+    end
   end
-
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/wasilak/elasticsearch-reroute-shards/releases/download/v1.0.0/elasticsearch-reroute-shards-linux-amd64.zip"
-    sha256 "39c2a6f5ac7eeb4f1102f53ae877ec2d6d9977b1c122b9512dac4b56bc235846"
-  end
-
-  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/wasilak/elasticsearch-reroute-shards/releases/download/v1.0.0/elasticsearch-reroute-shards-linux-arm64.zip"
-    sha256 "18a00e9ab8a58a999fcfcc4ef121af6c23f6c07f489005c350c3016d94c4e328"
-  end
-
-  # if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-  #   url "https://github.com/wasilak/elasticsearch-reroute-shards/releases/download/v1.0.0/elasticsearch-reroute-shards-linux-arm.zip"
-  #   sha256 ""
-  # end
 
   def install
     bin.install "elasticsearch-reroute-shards"
