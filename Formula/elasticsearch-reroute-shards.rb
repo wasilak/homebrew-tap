77e3ea6b436e43f715a5a19eb196489c3d6ae5ba
Piotr Boruc <piotr.m.boruc@gmail.com>
2026-05-16 18:59:54 +0200

    [elasticsearch-reroute-shards] Update to v1.0.1

--- Formula/elasticsearch-reroute-shards.rb
+++ Formula/elasticsearch-reroute-shards.rb
@@ -1,7 +1,7 @@
 class ElasticsearchRerouteShards < Formula
   desc "Elasticsearch shard rebalancing tool"
   homepage "https://github.com/wasilak/elasticsearch-reroute-shards"
-  version "1.0.0"
+  version "v1.0.1"
   license "MIT"
 
   on_macos do
@@ -10,18 +10,18 @@ class ElasticsearchRerouteShards < Formula
       sha256 "fac8ef4d8661b53b7d995637950f68abc6e847acd95ef723cce8824582896218"
     end
     on_arm do
-      url "https://github.com/wasilak/elasticsearch-reroute-shards/releases/download/v1.0.0/elasticsearch-reroute-shards-darwin-arm64.zip"
+      url "https://github.com/wasilak/elasticsearch-reroute-shards/releases/download/v1.0.1/elasticsearch-reroute-shards-darwin-arm64.zip"
       sha256 "e371a1bba33eaf369ce379f336732f5f7bdce5f3df51234afb03c1975f52a082"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/wasilak/elasticsearch-reroute-shards/releases/download/v1.0.0/elasticsearch-reroute-shards-linux-amd64.zip"
+      url "https://github.com/wasilak/elasticsearch-reroute-shards/releases/download/v1.0.1/elasticsearch-reroute-shards-linux-amd64.zip"
       sha256 "39c2a6f5ac7eeb4f1102f53ae877ec2d6d9977b1c122b9512dac4b56bc235846"
     end
     on_arm do
-      url "https://github.com/wasilak/elasticsearch-reroute-shards/releases/download/v1.0.0/elasticsearch-reroute-shards-linux-arm64.zip"
+      url "https://github.com/wasilak/elasticsearch-reroute-shards/releases/download/v1.0.1/elasticsearch-reroute-shards-linux-arm64.zip"
       sha256 "18a00e9ab8a58a999fcfcc4ef121af6c23f6c07f489005c350c3016d94c4e328"
     end
   end
